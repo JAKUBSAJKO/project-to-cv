ca23690f92726b5596cf2d97d75361575a7696f5
Jakub Sajko <jakubsajko.inf@gmail.com>
2022-03-16 17:27:39 +0100

    skills component version 1

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import Name from "./components/name/Name.js";
 import Education from "./components/education/Education";
 import Workexp from "./components/workExperience/WorkExp";
+import Skills from "./components/skills/Skills";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Name />
       <Education />
       <Workexp />
+      <Skills />
     </div>
   );
 }
---
+++ src/components/skills/Skills.js
@@ -0,0 +1,39 @@
+import React from "react";
+import "./Skills.css";
+// graphics
+import Me from "../../assets/skills-me.png";
+import HTML from "../../assets/skills-html.png";
+import CSS from "../../assets/skills-css.png";
+import JS from "../../assets/skills-js.png";
+import REACT from "../../assets/skills-react.png";
+import PY from "../../assets/skills-py.png";
+import GIT from "../../assets/skills-git.png";
+import BLEDNER from "../../assets/skills-blender.png";
+import FIGMA from "../../assets/skills-figma.png";
+
+const Skills = () => {
+  return (
+    <div className="skills-container">
+      <div className="skills-container-bg">
+        <div className="skills-header">
+          <h2>Umiejętności</h2>
+        </div>
+        <div className="skills-maps">
+          <div className="skills-maps-center">
+            <img src={Me} alt="" />
+            <img className="skills-icon" src={HTML} alt="" />
+            <img className="skills-icon" src={CSS} alt="" />
+            <img className="skills-icon" src={JS} alt="" />
+            <img className="skills-icon" src={REACT} alt="" />
+            <img className="skills-icon" src={PY} alt="" />
+            <img className="skills-icon" src={GIT} alt="" />
+            <img className="skills-icon" src={BLEDNER} alt="" />
+            <img className="skills-icon" src={FIGMA} alt="" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Skills;
