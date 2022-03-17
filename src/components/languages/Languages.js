f3fa3f05ba1bf9bb2a07bc2e55cfd1bd6b0a7916
Jakub Sajko <jakubsajko.inf@gmail.com>
2022-03-17 16:54:02 +0100

    language component v2

--- src/components/languages/Languages.js
+++ src/components/languages/Languages.js
@@ -1,12 +1,26 @@
 import React from "react";
 import "./Languages.css";
+import PL from "../../assets/language-pl.png";
+import ENG from "../../assets/language-eng.png";
 
 const Languages = () => {
   return (
     <div className="languages-container">
       <div className="languages-contain">
         <div className="languages-header">
-          <h2>Znajomość języków</h2>
+          <h1>Znajomość języków</h1>
+        </div>
+        <div className="languages-content">
+          <div className="languages-left">
+            <img src={PL} alt="" />
+            <h2>Język polski</h2>
+            <h3>język ojczysty</h3>
+          </div>
+          <div className="languages-right">
+            <img src={ENG} alt="" />
+            <h2>Język angielski</h2>
+            <h3>średnio zaawansowany niższy</h3>
+          </div>
         </div>
       </div>
     </div>
