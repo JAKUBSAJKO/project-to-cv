19ec129c94cf6b38dafa148fc7bb5892ca603746
Jakub Sajko <jakubsajko.inf@gmail.com>
2022-03-17 12:23:18 +0100

    contact component version 1

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -3,8 +3,66 @@ import "./Contact.css";
 
 const Contact = () => {
   return (
-    <div>
-      <h1>Contact</h1>
+    <div className="contact-container">
+      <div className="contact-header">
+        <h2>Kontakt</h2>
+      </div>
+      <div className="contact-content">
+        <div className="contact-left">
+          <div className="contact-left-mail">
+            <i class="fas fa-envelope"></i>
+            <h1>jakubsajko.inf@gmail.com</h1>
+          </div>
+          <div className="contact-left-phone">
+            <i class="fas fa-mobile-alt"></i>
+            <h1>515 982 498</h1>
+          </div>
+          <div className="contact-left-city">
+            <i class="fas fa-map-marker-alt"></i>
+            <h1>Gdańsk / Nowy Dwór Gdański</h1>
+          </div>
+        </div>
+        <div className="contact-right">
+          <form
+            id="contactform"
+            action="https://formsubmit.io/send/jakubsajko.inf@gmail.com"
+            method="POST"
+          >
+            <input
+              name="_formsubmit_id"
+              type="text"
+              className="honeypot"
+            ></input>
+            <input
+              type="text"
+              name="name"
+              id="name"
+              placeholder="Wpisz imię i nazwisko"
+            />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Wpisz email"
+            />
+            <input
+              type="text"
+              name="subjects"
+              id="subjects"
+              placeholder="Wpisz temat"
+            />
+            <textarea
+              name="msg"
+              id="msg"
+              rows="3"
+              placeholder="Wiadomość"
+            ></textarea>
+            <button type="submit" className="contact-btn" value="Submit">
+              Wyślij
+            </button>
+          </form>
+        </div>
+      </div>
     </div>
   );
 };
