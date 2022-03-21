fd6eba303229f8ac4d0c464da1ad127ad355a823
Jakub Sajko <jakubsajko.inf@gmail.com>
2022-03-21 18:44:40 +0100

    interests done

--- src/components/interests/Interests.js
+++ src/components/interests/Interests.js
@@ -7,6 +7,76 @@ const Interests = () => {
       <div className="interests-header">
         <h2>Zainteresowania</h2>
       </div>
+      <div className="interests-content">
+        <div className="interests-music">
+          <h2>Muzyka</h2>
+          <div className="interests-scroll-animation">
+            <div className="interests-info">
+              <h3>Zespół</h3>
+              <p>
+                Od kilku lat gram na gitarze oraz śpiewam w zespole muzycznym,
+                który założyłem <br />z kolegą. Muzykę komponowaną przeze mnie
+                można przypisać do klimatów rocka. Muzyka i słowa są dla mnie
+                tak samo ważne <br />w piosenkach. Teksty piszę <br />w języku
+                polskim.
+              </p>
+              <br />
+              <h3>Ulubione zespoły</h3>
+              <h3 className="top">Świat</h3>
+              <ul>
+                <li>Linkin Park</li>
+                <li>Bring me the horizon</li>
+                <li>Oasis</li>
+                <li>Placebo</li>
+                <li>Red hot chilli peppers</li>
+              </ul>
+              <h3 className="top">Polska</h3>
+              <ul>
+                <li>Ted Nemeth / Patryk Pietrzak</li>
+                <li>Happysad</li>
+                <li>Myslovitz</li>
+              </ul>
+            </div>
+          </div>
+          <i class="fas fa-caret-down down-arrow"></i>
+        </div>
+        <div className="interests-sport">
+          <h2>Sport</h2>
+          <div className="interests-scroll-animation">
+            <div className="interests-info">
+              <h3>Koszykówka</h3>
+              <p>
+                Jest to zdecydowanie mój ulubiony sport. Gra jest szybka, gdyż
+                każda z drużyna ma jedynie 24 sekundy na wykonanie akcji. Poza
+                oglądaniem lubię również grać w kosza. Ulubioną drużyną jest Los
+                Angeles Lakers, a ulubionym koszykarzem zawsze będzie Kobe
+                Bryant.
+              </p>
+              <br />
+              <h3>Formuła 1</h3>
+              <p>
+                Ciekawy sport, zdecydowanie inny niż reszta sportów. Walka
+                <br />z innymi zespołami, a także <br />z zespołowym kolegą to
+                coś czego nie spotykamy w żadnym innym sporcie. Wiele zależy{" "}
+                <br />
+                od kierowców, ale praktycznie tyle samo zależy od bolidu.
+                Formulą 1 zacząłem interesować się <br />
+                po obejrzeniu pierwszego sezonu serialu "Drive to survive".
+              </p>
+            </div>
+          </div>
+          <i class="fas fa-caret-down down-arrow"></i>
+        </div>
+        <div className="interests-book">
+          <h2>Książki</h2>
+          <p>
+            Ulubiony gatunek to zdecydowanie powieści kryminalne. Ruth Ware oraz
+            Stephen King to moi ulubieni autorzy, których styl mi odpowieda.
+            Jeżeli nie mam czasu na książki to staram się wybierać audiobooki
+            jak tło do zajęć.
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
