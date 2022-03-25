2cf17cd1e5ff14be54edce87d49692eeabb135ac
Jakub Sajko <jakubsajko.inf@gmail.com>
2022-03-25 18:24:27 +0100

    new skills component

--- src/components/skills/Skills.js
+++ src/components/skills/Skills.js
@@ -1,94 +1,38 @@
 import React from "react";
 import "./Skills.css";
 // graphics
-import Me from "../../assets/skills-me.png";
+import ME from "../../assets/skills-me.png";
 import HTML from "../../assets/skills-html.png";
 import CSS from "../../assets/skills-css.png";
 import JS from "../../assets/skills-js.png";
 import REACT from "../../assets/skills-react.png";
 import PY from "../../assets/skills-py.png";
 import GIT from "../../assets/skills-git.png";
-import BLEDNER from "../../assets/skills-blender.png";
+import BLENDER from "../../assets/skills-blender.png";
 import FIGMA from "../../assets/skills-figma.png";
-import { motion } from "framer-motion";
-
-// circle animation
-const cirlce = {
-  hover: {
-    scale: 1.1,
-    transition: {
-      yoyo: Infinity,
-    },
-  },
-};
 
 const Skills = () => {
   return (
     <div className="skills-container" id="skills">
-      <div className="skills-container-bg">
-        <div className="skills-header">
-          <h1>Umiejętności</h1>
+      <div className="skills-header">
+        <h1>Umiejętności</h1>
+      </div>
+      <hr className="title-line" />
+      <div className="skills-content">
+        <div className="skills-row-1">
+          <img src={HTML} alt="" />
+          <img src={CSS} alt="" />
+          <img src={JS} alt="" />
         </div>
-        <div className="skills-maps">
-          <div className="skills-maps-center">
-            <img src={Me} alt="" className="me-icon" />
-            <motion.img
-              className="skills-icon"
-              src={HTML}
-              alt=""
-              variants={cirlce}
-              whileHover="hover"
-            />
-            <motion.img
-              className="skills-icon"
-              src={CSS}
-              alt=""
-              variants={cirlce}
-              whileHover="hover"
-            />
-            <motion.img
-              className="skills-icon"
-              src={JS}
-              alt=""
-              variants={cirlce}
-              whileHover="hover"
-            />
-            <motion.img
-              className="skills-icon"
-              src={REACT}
-              alt=""
-              variants={cirlce}
-              whileHover="hover"
-            />
-            <motion.img
-              className="skills-icon"
-              src={PY}
-              alt=""
-              variants={cirlce}
-              whileHover="hover"
-            />
-            <motion.img
-              className="skills-icon"
-              src={GIT}
-              alt=""
-              variants={cirlce}
-              whileHover="hover"
-            />
-            <motion.img
-              className="skills-icon"
-              src={BLEDNER}
-              alt=""
-              variants={cirlce}
-              whileHover="hover"
-            />
-            <motion.img
-              className="skills-icon"
-              src={FIGMA}
-              alt=""
-              variants={cirlce}
-              whileHover="hover"
-            />
-          </div>
+        <div className="skills-row-2">
+          <img src={FIGMA} className="skill-row-left" alt="" />
+          <img src={ME} className="skill-row-me" alt="" />
+          <img src={REACT} alt="" />
+        </div>
+        <div className="skills-row-3">
+          <img src={BLENDER} alt="" />
+          <img src={GIT} alt="" />
+          <img src={PY} alt="" />
         </div>
       </div>
     </div>
